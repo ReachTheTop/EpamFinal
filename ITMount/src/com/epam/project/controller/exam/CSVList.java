878d67d931bd66a3b172f523b8f11a3595329ddd
V-Lobachov <hiken127@gmail.com>
2015-07-09 11:53:25 +0300

    файли екзаменів підтримують кирилицю

--- ITMount/src/com/epam/project/controller/exam/CSVList.java
+++ ITMount/src/com/epam/project/controller/exam/CSVList.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import com.epam.project.command.Action;
 import com.epam.project.db.connection.DBConnection;
@@ -21,63 +26,70 @@ public class CSVList implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet();
+		HSSFRow row = sheet.createRow(0);
+		
+		
+
+		
 		Integer exam_id = Integer.parseInt(request.getParameter("exam_id"));
-		response.setContentType("text/csv");
-		response.setHeader("Content-Disposition",
-				"attachment; filename=\"userDirectory.csv\"");
+		
 		ResourceBundle res = (ResourceBundle) request.getSession().getAttribute("bundle");
 		Connection connection = DBConnection.getConnection();
 		String query = "SELECT * FROM user AS u LEFT JOIN contact AS c ON u.id = c.user_id WHERE u.id IN (SELECT user_id FROM group_user WHERE exam_id = ?);";
 		PreparedStatement statement = null;
-
+		
 		try {
 			statement = connection.prepareStatement(query);
 			statement.setInt(1, exam_id);
 			ResultSet data = statement.executeQuery();
 
-			response.setContentType("text/csv");
-			String reportName = "GenerateCSV_Report_"
-					+ System.currentTimeMillis() + ".csv";
-			response.setHeader("Content-disposition", "attachment; "
-					+ "filename=" + reportName);
-
-			ArrayList<String> rows = new ArrayList<String>();
-
-			rows.add(res.getString("CSVList.surname"));
-			rows.add(";");
-			rows.add(res.getString("CSVList.firstname"));
-			rows.add(";");
-			rows.add(res.getString("CSVList.lastname"));
-			rows.add(";");
-			rows.add(res.getString("CSVList.email"));
-			rows.add(";");
-			rows.add(res.getString("CSVList.phone"));
-			rows.add("\n");
+			HSSFCell cell = row.createCell(0);
+			cell.setCellValue(res.getString("CSVList.surname"));
+			
+			cell = row.createCell(1);
+			cell.setCellValue(res.getString("CSVList.firstname"));
+			
+			cell = row.createCell(2);
+			cell.setCellValue(res.getString("CSVList.lastname"));
+			
+			cell = row.createCell(3);
+			cell.setCellValue(res.getString("CSVList.email"));
+			
+			cell = row.createCell(4);
+			cell.setCellValue(res.getString("CSVList.phone"));
+			
+			int i = 1;
 			while (data.next()) {
-				rows.add(data.getString("surname"));
-				rows.add(";");
-				rows.add(data.getString("name"));
-				rows.add(";");
-				rows.add(data.getString("midle_name"));
-				rows.add(";");
-				rows.add(data.getString("email"));
-				rows.add(";");
-				rows.add(data.getString("phone"));
-				rows.add("\n");
+				row = sheet.createRow(i);
+				
+				cell = row.createCell(0);
+				cell.setCellValue(data.getString("surname"));
+				
+				cell = row.createCell(1);
+				cell.setCellValue(data.getString("name"));
+				
+				cell = row.createCell(2);
+				cell.setCellValue(data.getString("midle_name"));
+				
+				cell = row.createCell(3);
+				cell.setCellValue(data.getString("email"));
+				
+				cell = row.createCell(4);
+				cell.setCellValue(data.getString("phone"));
+				
 			}
-
-			Iterator<String> iter = rows.iterator();
-			while (iter.hasNext()) {
-				String outputString = (String) iter.next();
-				response.getOutputStream().print(outputString);
-			}
-
-			response.getOutputStream().flush();
-
-		} catch (Exception e) {
-
-			System.out.println(e.toString());
+		
+		}catch(SQLException e){
+			e.printStackTrace();
 		}
+		
+		response.setContentType("application/vnd.ms-excel");
+		response.setHeader("Content-Disposition", "attachment; filename=filename.xls");
+		
+		workbook.write(response.getOutputStream()); // Write workbook to response.
+
 
 	}
 
